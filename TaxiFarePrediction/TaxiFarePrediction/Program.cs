d8a5f6bfbed414f35171aaa8c3cc35400d9c22f4
coding4funrocks <me@coding4fun.rocks>
2019-05-12 17:01:34 +0200

    Sample for taxi fare prediction

---
+++ TaxiFarePrediction/TaxiFarePrediction/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML;
+using PricePredictionML.Model.DataModels;
+using System;
+
+namespace PricePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeModel();
+        }
+
+        public static void ConsumeModel()
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.Passenger_count = 4;
+            input.Trip_distance = 2;
+
+            // Try model on sample data
+            ModelOutput result = predEngine.Predict(input);
+            Console.WriteLine("Result=" + result.Score);
+        }
+    }
+}
